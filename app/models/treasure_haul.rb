2239726a74dc3ba3a395dea95ec07920ed25a7aa
Seth Puckett <puckett.seth@gmail.com>
2018-09-04 15:08:52 -0400

    add models

--- app/models/treasure_haul.rb
+++ app/models/treasure_haul.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TreasureHaul
-  attr_accessor :cp, :sp, :ep, :gp, :pp
+  attr_accessor :cp, :sp, :ep, :gp, :pp, :treasure_objects, :magic_items
 
   # rubocop:disable Naming/UncommunicativeMethodParamName
   def initialize(cp: 0, sp: 0, ep: 0, gp: 0, pp: 0)
@@ -10,6 +10,8 @@ class TreasureHaul
     @ep = ep
     @gp = gp
     @pp = pp
+    @treasure_objects = []
+    @magic_items = []
   end
   # rubocop:enable Naming/UncommunicativeMethodParamName
 end
